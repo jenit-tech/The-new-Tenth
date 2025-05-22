68fa10972a3ba449467283236709c353ada46ff2
jenit-tech <jenitjosephjose@gmail.com>
2025-05-22 23:22:58 +0530

    added

--- components/Main/Footer.tsx
+++ components/Main/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="pl-9 md:pl-14 pr-4 md:pr-0    py-[40px] bg-white mt-5">
+    <div className="pl-9 md:pl-14 pr-4 md:pr-1    py-[40px] bg-white mt-5">
       <div className="grid grid-cols-1 md:grid-cols-12 gap-2">
         <div className="md:col-span-2">
 
